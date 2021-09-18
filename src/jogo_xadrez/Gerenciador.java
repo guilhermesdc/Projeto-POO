35277e0af4aca8098132ca330e7313e6f36e71bd
A-nita <nimoura1@gmail.com>
2021-09-18 14:06:50 -0300

    Gerenciador iniciado

--- src/jogo_xadrez/Gerenciador.java
+++ src/jogo_xadrez/Gerenciador.java
@@ -1,7 +1,34 @@
 package jogo_xadrez;
 
+import java.util.Scanner;
+
+
 public class Gerenciador {
+
     public static void main(String[] args) {
-        System.out.println("Main funcionando");
+        String nomeJogador1;
+        String corJogador1;
+        String nomeJogador2;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Bem-vindo Jogadores!");
+        System.out.print("Digite o nome do Jogador 1: ");
+        nomeJogador1 = in.nextLine();
+        System.out.print("\n "+ nomeJogador1+" digite B para escolher as pecas brancas, ou P para as pretas: ");
+        corJogador1 = in.nextLine();
+        System.out.print("\nDigite o nome do Jogador 2: ");
+        nomeJogador2 = in.nextLine();
+
+        if(corJogador1.equals("B")){
+            Jogo jogo = new Jogo(nomeJogador1, nomeJogador2);
+        }else{
+            Jogo jogo = new Jogo(nomeJogador2, nomeJogador1);
+        }
+
+
+//        System.out.println(j1.nome);
+//        System.out.println(j1.cor);
+//        System.out.println(j2.nome);
+//        System.out.println(j2.cor);
     }
 }
