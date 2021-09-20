70e32ad329425514ecf5f05ed5aea2fc5062a7bc
A-nita <nimoura1@gmail.com>
2021-09-20 11:58:32 -0300

    classe posicao concluida, e criaca de enum

--- src/jogo_xadrez/Posição.java
+++ src/jogo_xadrez/Posição.java
@@ -2,19 +2,28 @@ package jogo_xadrez;
 
 public class Posição {
 //    ATRIBUTES
+
+    private static final boolean branco = true;
+    private static final boolean preto = false;
+    public enum Pecas {
+        REI,DAMA,CAVALO,BISPO,TORRE,PEAO;
+    }
+    public enum Coluna{
+        A,B,C,D,E,F,G,H;
+    }
+
     private boolean cor;
     private boolean ocupada;
     private boolean corPeca;
-    private String peca;
-    private int linha;
-    private int coluna;
+    private int peca;
+    private final int linha;
+    private final int coluna;
 
 //    CONTRUCTOR
-
-    public Posição(boolean ocupada, String peca, int linha, int coluna) {
+    //padrao para
+    public Posição(boolean ocupada, boolean cor, int linha, int coluna) {
         this.cor = cor;
         this.ocupada = ocupada;
-        this.peca = peca;
         this.linha = linha;
         this.coluna = coluna;
     }
@@ -26,4 +35,39 @@ public class Posição {
 //    GETTERS & SETTERS
 
 
+    public boolean isCor() {
+        return cor;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public boolean isCorPeca() {
+        return corPeca;
+    }
+
+    public int getPeca() {
+        return peca;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    public void setCorPeca(boolean corPeca) {
+        this.corPeca = corPeca;
+    }
+
+    public void setPeca(int peca) {
+        this.peca = peca;
+    }
 }
--- src/jogo_xadrez/Tabuleiro.java
+++ src/jogo_xadrez/Tabuleiro.java
@@ -2,14 +2,36 @@ package jogo_xadrez;
 
 public class Tabuleiro {
       //      ATRIBUTES
-      private static final char[] colunas = {'a','b','c','d','e','f','g','h'};
-      private int[][] tabuleiro = new int[8][8];
+      private static final boolean BRANCO = true;
+      private static final boolean PRETO = false;
+    private static final int TAM = 8;
+
+    public enum Pecas {
+        REI,DAMA,CAVALO,BISPO,TORRE,PEAO;
+    }
+    public enum coluna{
+        A,B,C,D,E,F,G,H;
+    }
+      private Posição[][] tabuleiro;
 
 
 //      CONTRUCTOR
 
 
-//      METHODS
+    public Tabuleiro() {
+        for (int i = 0; i < TAM; i++) {
+            for (int j = 0; j < TAM; j++) {
+                if((i%2 == 0 && j%2 == 0) || (i%2 == 1 && j%2 == 1)){
+                    tabuleiro[i][j] = new Posição(false, BRANCO, i, j);
+                }
+                else{
+                    tabuleiro[i][j] = new Posição(false, PRETO, i, j);
+                }
+            }
+        }
+    }
+
+    //      METHODS
       public static void desenho(){
           for (int i = 0; i < 8; i++) {
               if(i%2 == 0){
@@ -29,6 +51,24 @@ public class Tabuleiro {
 
       }
 
+      public void colocarPecas(){
+        //pretas
+          for (int i = 6; i < 8; i++) {
+              for (int j = 0; j < 8; j++) {
+                  tabuleiro[i][j].setOcupada(true);
+                  tabuleiro[i][j].setCorPeca(PRETO);
+                  if(i==6){
+                      tabuleiro[i][j].setPeca(Pecas.PEAO.ordinal());
+                  }
+              }
+          }
+          //brancas
+          for (int i = 0; i < 2; i++) {
+              for (int j = 0; j < 8; j++) {
+
+              }
+          }
+      }
 
 //      GETTERS & SETTERS
 
