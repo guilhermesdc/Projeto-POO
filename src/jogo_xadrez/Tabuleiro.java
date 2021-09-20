3d9933528993587f98473392f9d826d6ea2fa9b2
A-nita <nimoura1@gmail.com>
2021-09-20 12:19:12 -0300

    frufru

--- src/jogo_xadrez/Tabuleiro.java
+++ src/jogo_xadrez/Tabuleiro.java
@@ -1,76 +1,70 @@
 package jogo_xadrez;
 
 public class Tabuleiro {
-      //      ATRIBUTES
-      private static final boolean BRANCO = true;
-      private static final boolean PRETO = false;
+    /* ATRIBUTES */
+    private static final boolean BRANCO = true;
+    private static final boolean PRETO = false;
     private static final int TAM = 8;
 
     public enum Pecas {
-        REI,DAMA,CAVALO,BISPO,TORRE,PEAO;
+        REI, DAMA, CAVALO, BISPO, TORRE, PEAO;
     }
-    public enum coluna{
-        A,B,C,D,E,F,G,H;
+
+    public enum Coluna {
+        A, B, C, D, E, F, G, H;
     }
-      private Posição[][] tabuleiro;
 
+    private Posição[][] tabuleiro;
 
-//      CONTRUCTOR
-
-
+    /* CONTRUCTOR */
     public Tabuleiro() {
         for (int i = 0; i < TAM; i++) {
             for (int j = 0; j < TAM; j++) {
-                if((i%2 == 0 && j%2 == 0) || (i%2 == 1 && j%2 == 1)){
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
                     tabuleiro[i][j] = new Posição(false, BRANCO, i, j);
-                }
-                else{
+                } else {
                     tabuleiro[i][j] = new Posição(false, PRETO, i, j);
                 }
             }
         }
     }
 
-    //      METHODS
-      public static void desenho(){
-          for (int i = 0; i < 8; i++) {
-              if(i%2 == 0){
-                  System.out.println("|-------|-------|-------|-------|-------|-------|-------|-------|");
-                  System.out.println("|       |#######|       |#######|       |#######|       |#######|");
-                  System.out.println("|       |#######|       |#######|       |#######|       |#######|");
-              }
-              else{
-                  System.out.println("|-------|-------|-------|-------|-------|-------|-------|-------|");
-                  System.out.println("|#######|       |#######|       |#######|       |#######|       |");
-                  System.out.println("|#######|       |#######|       |#######|       |#######|       |");
-              }
+    /* METHODS */
+    public static void desenho() {
+        for (int i = 0; i < 8; i++) {
+            if (i % 2 == 0) {
+                System.out.println("|-------|-------|-------|-------|-------|-------|-------|-------|");
+                System.out.println("|       |#######|       |#######|       |#######|       |#######|");
+                System.out.println("|       |#######|       |#######|       |#######|       |#######|");
+            } else {
+                System.out.println("|-------|-------|-------|-------|-------|-------|-------|-------|");
+                System.out.println("|#######|       |#######|       |#######|       |#######|       |");
+                System.out.println("|#######|       |#######|       |#######|       |#######|       |");
+            }
+        }
+        System.out.println("|-------|-------|-------|-------|-------|-------|-------|-------|");
+    }
 
-          }
-              System.out.println("|-------|-------|-------|-------|-------|-------|-------|-------|");
+    public void colocarPecas() {
+        // Pretas
+        for (int i = 6; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                tabuleiro[i][j].setOcupada(true);
+                tabuleiro[i][j].setCorPeca(PRETO);
+                if (i == 6) {
+                    tabuleiro[i][j].setPeca(Pecas.PEAO.ordinal());
+                }
+            }
+        }
+        // Brancas
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 8; j++) {
 
+            }
+        }
+    }
 
-      }
-
-      public void colocarPecas(){
-        //pretas
-          for (int i = 6; i < 8; i++) {
-              for (int j = 0; j < 8; j++) {
-                  tabuleiro[i][j].setOcupada(true);
-                  tabuleiro[i][j].setCorPeca(PRETO);
-                  if(i==6){
-                      tabuleiro[i][j].setPeca(Pecas.PEAO.ordinal());
-                  }
-              }
-          }
-          //brancas
-          for (int i = 0; i < 2; i++) {
-              for (int j = 0; j < 8; j++) {
-
-              }
-          }
-      }
-
-//      GETTERS & SETTERS
+    /* GETTERS & SETTERS */
 
 
 }
